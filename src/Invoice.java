5131240f6ba8aeb365bc195693c9e08872350910
AndSimi <71438281+AndSimi@users.noreply.github.com>
2020-10-22 13:28:12 +0200

    Update Invoice.java
    small comments

--- src/Invoice.java
+++ src/Invoice.java
@@ -9,13 +9,16 @@ import java.util.Date;
 public class Invoice {
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
-
+        
+        //Getting the input from user
+        
         System.out.println("Please insert the amount due: ");
-        double AmountDue= Double.parseDouble(input.nextLine());
+        //Converting from String to double, because if we use nextDouble, the database skips the next input
         System.out.println("Please insert the due date: ");
         String date= input.nextLine();
 
-
+        //Inserting the given values into our database
+        
         DB.insertSQL("Insert into tblInvoices Values('"+date+"',"+AmountDue+")");
     }
 }
