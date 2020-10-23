04fea462adb7f13ffbe909e6ae732f4af2377666
AndSimi <71438281+AndSimi@users.noreply.github.com>
2020-10-23 13:51:54 +0200

    Update Payments.java
    javaDoc

--- src/Payments.java
+++ src/Payments.java
@@ -2,7 +2,23 @@ import java.sql.SQLException;
 import java.util.Date;
 import java.util.Scanner;
 
+
 public class Payments {
+    
+    /**
+    * @author Ibraheem David Andrej
+    * The class payments asks the user to input information about the recieved payment
+    * Customer Id is used to assign a specific payment to specific customer 
+    * The payment date is used  later on to check if the payment was made on time
+    * PaymentType is used in order to generate the daily cash flow in DailyTotals class
+    * Amount is used to check if the full amount is paid
+    * Reminder count is used to keep track of how many reminders have been sent and how much late fee should be added to the invoice
+    */
+    
+    
+    
+    
+    
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
 
