ca15c0d75af9f4dac0c3fec397ceb29a9b995f7c
AndSimi <71438281+AndSimi@users.noreply.github.com>
2020-10-23 13:15:20 +0200

    Update Customers.java
    added java doc

--- src/Customers.java
+++ src/Customers.java
@@ -4,7 +4,11 @@ public class Customers {
 
     /**
      *
-     * @author Andrej :0
+     * @author Andrej David Ibraheem
+     * Class customers is used to get information about the customer, mainly used in order to create a new client
+     * The program asks the user to input name, address, email, phone number, and if the customer is active/inactive
+     * The activity is used in order to determine if the customers account is still valid
+     * If the customer is inactive it means that the customer has moved or has recieved 3 reminders about the bill 
      */
 
     public static void main (String [] args){
