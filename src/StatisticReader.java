9ed4fbe727099fe869e0f5542a542114de917fc0
AndSimi <71438281+AndSimi@users.noreply.github.com>
2020-10-23 10:05:52 +0200

    Update StatisticReader.java
    comments

--- src/StatisticReader.java
+++ src/StatisticReader.java
@@ -2,18 +2,25 @@ import java.util.Scanner;
 
 public class StatisticReader {
     public static void main(String[] args) {
+        
+        //Getting user inputs
 
         Scanner input= new Scanner(System.in);
         System.out.println("Please insert a date for consumption");
         String inputDate= input.nextLine();
-
+        
+        //Collecting data from our database
+        
         DB.selectSQL("Select Sum (fldConsumption) from tblMeters where fldDate='"+inputDate+"'");
-
+    
+        //Displaying the data to user
+        
         do{
             String data = DB.getDisplayData();
             if (data.equals(DB.NOMOREDATA)){
                 break;
             }else{
+               
                 System.out.print("The total water consumption on " +inputDate +  " is " +data);
             }
         } while(true);
